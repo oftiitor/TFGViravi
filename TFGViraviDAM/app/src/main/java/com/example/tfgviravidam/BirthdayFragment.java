9a9f50ee8b8e2388d8292b8999833090caad694f
raularribasc <raularribasc@gmail.com>
2023-03-09 12:40:35 +0100

    App Start

---
+++ TFGViraviDAM/app/src/main/java/com/example/tfgviravidam/BirthdayFragment.java
@@ -0,0 +1,32 @@
+package com.example.tfgviravidam;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class BirthdayFragment extends Fragment {
+
+    Button btn;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_birthday, container, false);
+        btn = view.findViewById(R.id.btnBirth);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_birthdayFragment_to_phoneFragment);
+            }
+        });
+        return view;
+    }
+}
